31a15163f140a89c5c7bd0dd00f88989c0f6dfc8
Billie Zhang <billiecoder@gmail.com>
2014-05-04 15:20:03 +0800

    i

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kitty::Application.config.secret_key_base = 'db5af33a8b127446e5149df22a432b287860612fa90745f6413e9c5b68d172b8b151bb995a993ca4f60d0945d9e4923fe57620071f2008766656ede9ec39acf4'
